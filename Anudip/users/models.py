824cc479a113fd32b1e6f64cc52e56a9be2f88e6
Jiten <jitensidhpura2000@gmail.com>
2020-08-16 11:57:20 +0530

    Basic Setup Complete

---
+++ Anudip/users/models.py
@@ -0,0 +1,5 @@
+# https://github.com/IndiaCFG3/team-63
+
+from django.db import models
+
+# Create your models here.
---
+++ Anudip/users/views.py
@@ -0,0 +1,17 @@
+# https://github.com/IndiaCFG3/team-63
+
+from django.shortcuts import render
+import os
+import json
+from django.http import HttpResponse, JsonResponse
+
+from django.core.paginator import Paginator
+from .models import *
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.utils.html import format_html
+from rest_framework.views import APIView
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
