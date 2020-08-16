8f75f6cb8cf74d81ad8ded4bfa6f8a8f4cb831f7
Jiten <jitensidhpura2000@gmail.com>
2020-08-16 12:25:04 +0530

    Type of User added

--- Anudip/users/models.py
+++ Anudip/users/models.py
@@ -1,5 +1,22 @@
 # https://github.com/IndiaCFG3/team-63
 
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Application_User(models.Model):
+
+	user = models.OneToOneField(User,on_delete=models.CASCADE)
+
+	choices = [
+	
+		('Manager', 'Manager'),
+		('Mobilizer',  'Mobilizer')
+	
+	]
+
+	type_of_user = models.CharField(max_length=9,choices = choices, default='Mobilizer')
+
+	def __str__(self):
+		return f'{self.user.username} Application User'
